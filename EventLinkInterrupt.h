ae94b557115148610dbd3a1d01e58ccd120f12e8
David <6957239+delta-G@users.noreply.github.com>
2023-10-19 15:13:48 -0500

    added a little documentation

--- EventLinkInterrupt.h
+++ EventLinkInterrupt.h
@@ -26,8 +26,30 @@ EventLinkInterrupt.h
 #include "IRQManager.h"
 #include "RTC.h"
 
-
+/*
+*
+*  attachEventLinkInterrupt(uint8_t eventCode, Irq_f func);
+*  attaches an interrupt handler in the event link controller
+*
+*  eventCode - The 8 bit code for the event link (See User's Manual Table 13.4)
+*  func - void () function to attach as a handler. 
+*
+*  returns int - the index of the event link that was used. 
+*                you will need this in the ISR to reset the interrupt
+*/
 int attachEventLinkInterrupt(uint8_t eventCode, Irq_f func = nullptr);
+
+/*
+*
+*  resetEventLink(int eventLinkIndex);
+*  clears the IR flag in the event link register to clear the interrupt
+*  This must be called from the interrupt handler or the board will hang
+*  There is probably also another interrupt flag for the event you linked
+*
+*  eventLinkIndex - The index of the event in the ELC.
+*                   returned from attachEventLinkInterrupt
+*
+*/
 void resetEventLink(int eventLinkIndex);
 
 
